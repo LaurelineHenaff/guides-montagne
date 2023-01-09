3964b3c2e009e73a2471e25aece31071a21e86ed
arpicode <arpicode@gmail.com>
2023-01-09 15:03:25 +0100

    Add `sommets`, `vallees` models

--- app/Models/Sommet.php
+++ app/Models/Sommet.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Sommet extends Model
 {
     use HasFactory;
+
+    protected $primaryKey = 'code_Sommets';
 }
--- app/Models/Vallee.php
+++ app/Models/Vallee.php
@@ -5,9 +5,9 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Sommets extends Model
+class Vallee extends Model
 {
     use HasFactory;
 
-    protected $primaryKey = 'code_Sommets';
+    protected $primaryKey = 'code_Vallees';
 }
